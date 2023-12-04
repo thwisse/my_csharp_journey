0eb96d01132089c0d62bd0b0859e772a2a14bb93
thwisse <thwisse@thwisse.com>
2023-12-04 15:52:37 +0300

    update

---
+++ my_console_apps/exercise03.cs
@@ -0,0 +1,33 @@
+﻿namespace my_console_apps
+{
+    internal class exercise03
+    {
+        static void Main3(string[] args)
+        {
+            //Bilgisayarin random tuttugu 10 sayidan cift ve tek olanlarin sayisini gosteren program. 
+
+            int sayi, tekAdet = 0, ciftAdet = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sayi = rnd.Next(1, 100);
+
+                Console.WriteLine(sayi);
+
+                if (sayi % 2 == 0)
+                {
+                    ciftAdet++;
+                }
+                else
+                {
+                    tekAdet++;
+                }
+            }
+            Console.WriteLine("Tek adet: {0}, Cift Adet: {1}", tekAdet, ciftAdet);
+            
+            char ch = Console.ReadKey(true).KeyChar;
+        }
+    }
+}
